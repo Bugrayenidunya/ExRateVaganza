69f4fd7e2ae53db55ec4404325e55942212ca7ed
Enes Buğra Yenidünya <bugrayenidunya@gmail.com>
2023-10-11 13:58:15 +0300

    Add alert manager

---
+++ ExRateVaganza/ExRateVaganza/Managers/AlertManager/AlertManager.swift
@@ -0,0 +1,40 @@
+//
+//  AlertManager.swift
+//  ExRateVaganza
+//
+//  Created by Enes Buğra Yenidünya on 11.10.2023.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - AlertShowable
+protocol AlertShowable {
+    var controller: UIViewController? { get set }
+    func showAlert(with error: NetworkError)
+}
+
+// MARK: - AlertManager
+final class AlertManager: AlertShowable {
+    
+    // MARK: Properties
+    static let shared: AlertShowable = AlertManager.init()
+    
+    weak var controller: UIViewController?
+    
+    // MARK: Init
+    private init() { }
+    
+    func showAlert(with error: NetworkError) {
+        guard let controller = controller else { return }
+        
+        let alert = UIAlertController(title: "", message: error.localizedDescription, preferredStyle: .alert)
+
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: { _ in
+        }))
+
+        DispatchQueue.main.async {
+            controller.present(alert, animated: true, completion: nil)
+        }
+    }
+}
