87148ce86f5a21c86591c17bfc10ea53f3a08dd8
Enes Buğra Yenidünya <bugrayenidunya@gmail.com>
2023-10-11 14:16:47 +0300

    Add String extension

---
+++ ExRateVaganza/ExRateVaganza/Utilities/Extensions/String+Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  String+Extensions.swift
+//  ExRateVaganza
+//
+//  Created by Enes Buğra Yenidünya on 11.10.2023.
+//
+
+import Foundation
+
+extension String {
+    static let empty = ""
+}
