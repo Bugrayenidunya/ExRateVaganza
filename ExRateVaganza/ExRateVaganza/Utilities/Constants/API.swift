c9fa5ce1f0442a601c34deab78f8478f5ff3d29e
Enes Buğra Yenidünya <bugrayenidunya@gmail.com>
2023-10-11 14:16:38 +0300

    Add constants

---
+++ ExRateVaganza/ExRateVaganza/Utilities/Constants/API.swift
@@ -0,0 +1,16 @@
+//
+//  API.swift
+//  ExRateVaganza
+//
+//  Created by Enes Buğra Yenidünya on 11.10.2023.
+//
+
+import Foundation
+
+extension Constant {
+    class API {
+        static let ticker = "ticker"
+        static let klineData = "kline-data"
+        static let baseUrl = "https://docs.btcturk.com/public-endpoints/"
+    }
+}
---
+++ ExRateVaganza/ExRateVaganza/Utilities/Constants/Constant.swift
@@ -0,0 +1,10 @@
+//
+//  Constant.swift
+//  ExRateVaganza
+//
+//  Created by Enes Buğra Yenidünya on 11.10.2023.
+//
+
+import Foundation
+
+class Constant { }
