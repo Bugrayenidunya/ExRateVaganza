3f08685c5fc30628b1a0ae12d22ba54e4219da7c
Enes Buğra Yenidünya <bugrayenidunya@gmail.com>
2023-10-11 13:02:55 +0300

    Make app to be able to work without Storyboard

---
+++ ExRateVaganza/ExRateVaganza/App/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  ExRateVaganza
+//
+//  Created by Enes Buğra Yenidünya on 11.10.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    // MARK: Properties
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
