8161979d9b8eb161b7857c1ef5544c154b36bbe8
Enes Buğra Yenidünya <bugrayenidunya@gmail.com>
2023-10-14 00:42:25 +0300

    Add UserDefaults constants

---
+++ ExRateVaganza/ExRateVaganza/Utilities/Constants/UserDefault.swift
@@ -0,0 +1,14 @@
+//
+//  UserDefault.swift
+//  ExRateVaganza
+//
+//  Created by Enes Buğra Yenidünya on 13.10.2023.
+//
+
+import Foundation
+
+extension Constant {
+    class UserDefault {
+        static let favorites = "Favorites"
+    }
+}
