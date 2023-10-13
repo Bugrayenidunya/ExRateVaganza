48c5338b51ea90e0511723c5cb48e5fb732f2a0f
Enes Buğra Yenidünya <bugrayenidunya@gmail.com>
2023-10-14 01:20:16 +0300

    Add GetAllPairsResponseModel

---
+++ ExRateVaganza/ExRateVaganza/Models/Response/GetAllPairsResponseModel.swift
@@ -0,0 +1,12 @@
+//
+//  GetAllPairsResponseModel.swift
+//  ExRateVaganza
+//
+//  Created by Enes Buğra Yenidünya on 12.10.2023.
+//
+
+import Foundation
+
+class GetAllPairsResponseModel: ResponseModel<[Pair]> {
+    
+}
