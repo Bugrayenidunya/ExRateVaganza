60dbbd7dcbca58ab884444e427fee49eaa6c9ffa
Enes Buğra Yenidünya <bugrayenidunya@gmail.com>
2023-10-14 00:42:12 +0300

    Fix base url constant parameter

--- ExRateVaganza/ExRateVaganza/Utilities/Constants/API.swift
+++ ExRateVaganza/ExRateVaganza/Utilities/Constants/API.swift
@@ -11,6 +11,6 @@ extension Constant {
     class API {
         static let ticker = "ticker"
         static let klineData = "kline-data"
-        static let baseUrl = "https://docs.btcturk.com/public-endpoints/"
+        static let baseUrl = "https://api.btcturk.com/api/v2/"
     }
 }
