77fd31f11d4f8e099c3f60f30908e9523a70cf66
Enes Buğra Yenidünya <bugrayenidunya@gmail.com>
2023-10-14 01:23:20 +0300

    Update HomeBuilder

--- ExRateVaganza/ExRateVaganza/Modules/Home/Builder/HomeBuilder.swift
+++ ExRateVaganza/ExRateVaganza/Modules/Home/Builder/HomeBuilder.swift
@@ -10,11 +10,26 @@ import Foundation
 final class HomeBuilder {
     class func build() -> HomeController {
         let networkManager = NetworkManager(session: .shared)
-        let alertManager = AlertManager.shared
+        var alertManager = AlertManager.shared
         let loadingManager = LoadingManager.shared
+        let pairAPI = PairAPI(networkManager: networkManager)
+        let userDefaultsManager = UserDefaultsManager.shared
         let router = HomeRouter()
-        let viewModel = HomeViewModel(router: router, loadingManager: loadingManager, alertManager: alertManager, networkManager: networkManager)
+        
+        let viewModel = HomeViewModel(
+            router: router,
+            loadingManager: loadingManager,
+            alertManager: alertManager,
+            pairAPI: pairAPI,
+            userDefaultsManager: userDefaultsManager,
+            requestModel: GetAllPairsRequestModel()
+        )
+        
         let controller = HomeController(viewModel: viewModel)
+        
+        viewModel.output = controller
+        router.controller = controller
+        alertManager.controller = controller
         
         return controller
     }
