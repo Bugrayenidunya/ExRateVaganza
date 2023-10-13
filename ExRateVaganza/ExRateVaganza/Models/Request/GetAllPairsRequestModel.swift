7f514fa0785ff577783faf0f397e6c4c10275639
Enes Buğra Yenidünya <bugrayenidunya@gmail.com>
2023-10-14 01:20:30 +0300

    Add GetAllPairsRequestModel.swift

---
+++ ExRateVaganza/ExRateVaganza/Models/Request/GetAllPairsRequestModel.swift
@@ -0,0 +1,20 @@
+//
+//  GetAllPairsRequestModel.swift
+//  ExRateVaganza
+//
+//  Created by Enes Buğra Yenidünya on 12.10.2023.
+//
+
+import Foundation
+
+class GetAllPairsRequestModel: RequestModel {
+    
+    // MARK: Properties
+    override var method: RequestMethod {
+        .get
+    }
+    
+    override var path: String {
+        Constant.API.ticker
+    }
+}
