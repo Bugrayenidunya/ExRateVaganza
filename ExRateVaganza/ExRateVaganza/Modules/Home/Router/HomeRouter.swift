0241be9d4c39f78340582d6db09af3ed9006ad43
Enes Buğra Yenidünya <bugrayenidunya@gmail.com>
2023-10-15 00:40:01 +0300

    Add navigate to detail in HomeRouter

--- ExRateVaganza/ExRateVaganza/Modules/Home/Router/HomeRouter.swift
+++ ExRateVaganza/ExRateVaganza/Modules/Home/Router/HomeRouter.swift
@@ -9,10 +9,23 @@ import UIKit
 
 protocol HomeRouting {
     var controller: UIViewController? { get set }
+    
+    func navigateToDetail(_ klineDataProvider: KlineDataRequestProvider)
 }
 
 final class HomeRouter: HomeRouting {
     
     // MARK: Properties
     weak var controller: UIViewController?
+    
+    private var navigationController: UINavigationController? {
+        controller?.navigationController
+    }
+    
+    func navigateToDetail(_ klineDataProvider: KlineDataRequestProvider) {
+        guard let navigationController else { return }
+        
+        let detailController = DetailBuilder.build(klineDataProvider: klineDataProvider)
+        navigationController.show(detailController, sender: controller)
+    }
 }
