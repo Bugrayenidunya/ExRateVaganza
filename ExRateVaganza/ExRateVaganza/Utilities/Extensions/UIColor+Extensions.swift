5bd6ff8d355f8cb5c0415e67216a09065a34944f
Enes Buğra Yenidünya <bugrayenidunya@gmail.com>
2023-10-16 11:54:09 +0300

    Add UIColor extensions

---
+++ ExRateVaganza/ExRateVaganza/Utilities/Extensions/UIColor+Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  ExRateVaganza
+//
+//  Created by Enes Buğra Yenidünya on 16.10.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static let vaganzaBlue = UIColor(named: "vaganzaBlue")!
+    static let vaganzaDarkerBlue = UIColor(named: "vaganzaDarkerBlue")!
+    static let vaganzaGray = UIColor(named: "vaganzaGray")!
+    static let vaganzaGreen = UIColor(named: "vaganzaGreen")!
+    static let vaganzaRed = UIColor(named: "vaganzaRed")!
+    static let vaganzaWhite = UIColor(named: "vaganzaWhite")!
+}
