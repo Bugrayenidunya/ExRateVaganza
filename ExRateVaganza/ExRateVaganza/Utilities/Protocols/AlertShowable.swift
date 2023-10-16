c5460a49d7b5380749e0e59a34c4741723275f07
Enes Buğra Yenidünya <bugrayenidunya@gmail.com>
2023-10-16 14:15:10 +0300

    Convert AlertManager to AlertShowable protocol only

--- ExRateVaganza/ExRateVaganza/Utilities/Protocols/AlertShowable.swift
+++ ExRateVaganza/ExRateVaganza/Utilities/Protocols/AlertShowable.swift
@@ -10,31 +10,18 @@ import UIKit
 
 // MARK: - AlertShowable
 protocol AlertShowable {
-    var controller: UIViewController? { get set }
     func showAlert(with error: NetworkError)
 }
 
-// MARK: - AlertManager
-final class AlertManager: AlertShowable {
-    
-    // MARK: Properties
-    static let shared: AlertShowable = AlertManager.init()
-    
-    weak var controller: UIViewController?
-    
-    // MARK: Init
-    private init() { }
-    
+extension AlertShowable where Self: UIViewController {
     func showAlert(with error: NetworkError) {
-        guard let controller = controller else { return }
-        
         let alert = UIAlertController(title: "", message: error.description, preferredStyle: .alert)
 
         alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: { _ in
         }))
 
         DispatchQueue.main.async {
-            controller.present(alert, animated: true, completion: nil)
+            self.present(alert, animated: true, completion: nil)
         }
     }
 }
